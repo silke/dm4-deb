381b12a06eface31f8f397da18926cda04cdea76
Silke Meyer <silke@silkemeyer.net>
2014-02-22 20:32:20 +0100

    Files for DeepaMehta 4.2 and new version of packaging script

--- dm-package.sh
+++ dm-package.sh
@@ -6,26 +6,48 @@ DOWNLOADDIR="/home/silke/Downloads"
 TMPDIR="/tmp"
 WORKDIR="/home/silke/DeepaMehta/dm4-deb"
 
-## 4.1 -> 4.2 upgrade: dependency zu dm4-karaf einbauen.
-## db nicht mehr verschieben, sondern behalten
 
 if [ "$2" == "" ]; then
 	echo "Call this script with a version number, e.g. dm-package.sh 405"
 fi
-#nur zahlen
+# We only have numbers here
 
-#Punkte einsetzen
-NEWVERSIONNUMBER="${NEWVERSION:0:1}.${NEWVERSION:1:1}.${NEWVERSION:2:1}${NEWVERSION:3:1}"
-echo $NEWVERSIONNUMBER
-cd $WORKDIR
-pwd
+# insert the dots (for two and three digit version numbers)
+# counting: from where do you count and how many chars belong together?
 
-#Punkte einsetzen
+# for the new version no.
+if [ ${#NEWVERSION} -ge 3 ]; then
+		NEWVERSIONNUMBER="${NEWVERSION:0:1}.${NEWVERSION:1:1}.${NEWVERSION:2:1}${NEWVERSION:3:1}"
+#		echo $NEWVERSIONNUMBER
+	elif [ ${#NEWVERSION} -eq 2 ]; then
+		NEWVERSIONNUMBER="${NEWVERSION:0:1}.${NEWVERSION:1:1}"
+fi
+
+
+# insert dots for previous version no.
 if [ "${OLDVERSION:2:1}" == "" ]; then
 	OLDVERSIONNUMBER="${OLDVERSION:0:1}.${OLDVERSION:1:1}"
 else
 	OLDVERSIONNUMBER="${OLDVERSION:0:1}.${OLDVERSION:1:1}.${OLDVERSION:2:1}${OLDVERSION:3:1}"
 fi
+
+echo $OLDVERSIONNUMBER
+echo $NEWVERSIONNUMBER
+
+cd $WORKDIR
+
+echo "Version number correct?"
+
+sleep 5
+
+# remove existing tmp files
+if [ -d /tmp/deepamehta-${NEWVERSIONNUMBER} ]; then
+	rm -rf /tmp/deepamehta-${NEWVERSIONNUMBER}
+fi
+
+ls /tmp
+
+sleep 2
 
 cp -a $WORKDIR/deepamehta_package_template $WORKDIR/deepamehta-$NEWVERSIONNUMBER
 #gibts das schon?
@@ -41,18 +63,17 @@ fi
 
 unzip $DOWNLOADDIR/deepamehta-$NEWVERSIONNUMBER.zip -d $TMPDIR
 
+# gogo shell loeschen
+rm -rf $TMPDIR/deepamehta-$NEWVERSIONNUMBER/bundle/org.apache.felix.gogo.*
+
 # ins Buildverzeichnis wechseln
 cd $WORKDIR/deepamehta-$NEWVERSIONNUMBER
-
-
 
 cp $TMPDIR/deepamehta-$NEWVERSIONNUMBER/bin/* bin/bin/
 
 cp $TMPDIR/deepamehta-$NEWVERSIONNUMBER/bundle/* bin/bundle/
 cp $TMPDIR/deepamehta-$NEWVERSIONNUMBER/*.txt .
 
-# gogo shell loeschen
-rm -rf $TMPDIR/deepamehta-NEWVERSIONNUMBER/bin/bundle/org.apache.felix.gogo.*
 
 # Versionsnummer in preinst hochsetzen
 cat $WORKDIR/deepamehta_package_template/debian/preinst  | sed "s/^DMVERSION\=XXX/DMVERSION\=${NEWVERSIONNUMBER}/" > debian/preinst
